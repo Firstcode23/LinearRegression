f2cfb4992ba2e78edf5b73041782f91df71a5009
Firstcode23 <50323664+Firstcode23@users.noreply.github.com>
2021-05-09 18:46:30 +0530

    Add files via upload

--- Big Mart Sales Pridiction.py
+++ Big Mart Sales Pridiction.py
@@ -10,7 +10,10 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -65,8 +68,38 @@ def svm_regression(data):
     svm_cal.fit(X_train,y_train)
     y_predict = svm_cal.predict(X_test)
     #print('Prececison and Recall Score is :' ,precision_recall_fscore_support(y_test,y_predict,average = 'micro'))
-    print(svm_cal.score(X_train,y_train))
-    print(svm_cal.score(X_test,y_test))
+    r_score =r2_score(y_test, y_predict)
+    print(r_score)
+    #print(svm_cal.score(X_train,y_train))
+    #print(svm_cal.score(X_test,y_test))
+
+def decesion_regressor(data,md):
+    dt_regression = DecisionTreeRegressor(max_depth = md, min_samples_leaf= 100)
+    X = data.drop(['Item_Identifier','Item_Visibility','Item_Outlet_Sales'],axis = 1)
+    y = data['Item_Outlet_Sales']
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+    dt_regression.fit(X_train,y_train)
+    y_dt_predict = dt_regression.predict(X_test)
+    r_dt_score =r2_score(y_test, y_dt_predict)
+    print(r_dt_score)
+
+def random_regressor(data,n_estimator,md):
+    #parameters_grid =  {'bootstrap' : [True], 'max_depth': [5,10,None],'n_estimators':[10,20,30]}
+    rf_regression = RandomForestRegressor(n_estimators= n_estimator, max_depth=md)
+    #rf_regression = RandomForestRegressor(random_state = 42)
+    #grid_search = GridSearchCV(estimator = rf_regression,param_grid = parameters_grid,cv=3,n_jobs = 1,verbose = 0,return_train_score =  True)
+    X = data.drop(['Item_Identifier','Item_Visibility','Item_Outlet_Sales'],axis = 1)
+    y = data['Item_Outlet_Sales']
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+    rf_regression.fit(X_train,y_train)
+    #grid_search.fit(X_train,y_train)
+    #print(grid_search.best_params_)
+    #print(best_grid.score(X_test, y_test))
+    y_rf_regression_predict = rf_regression.predict(X_test)
+    rf_regression_r_score = r2_score(y_test,y_rf_regression_predict)
+    print(rf_regression_r_score)
+
+
 
 
 training_data = read_data()
@@ -76,6 +109,9 @@ label_data= label_encoder(im_data)
 df_corelation(im_data)
 df_anova(label_data)
 svm_regression(label_data)
+decesion_regressor(label_data,10)
+random_regressor(label_data,30,5)
+
 
 
 
